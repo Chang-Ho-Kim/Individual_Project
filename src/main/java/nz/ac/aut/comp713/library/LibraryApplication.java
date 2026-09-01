8358b901a368361517922c83606208126aedc8f7
Chang-Ho-Kim <fwr3521@autuni.ac.nz>
2026-09-02 10:24:31 +1200

    add openapi for the project

--- src/main/java/nz/ac/aut/comp713/library/LibraryApplication.java
+++ src/main/java/nz/ac/aut/comp713/library/LibraryApplication.java
@@ -3,6 +3,16 @@ package nz.ac.aut.comp713.library;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+
 @ApplicationPath("/api/v1")
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Library Management API",
+        version = "1.0",
+        description = "REST API for the Library Management System"
+    )
+)
 public class LibraryApplication extends Application {
 }
